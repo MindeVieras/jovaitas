65ece1206386ccac450e8813c0270c5655ec3613
Minde Vieras <mindevieras@gmail.com>
2019-02-15 19:10:33 +0000

    returns conditions link

--- sites/all/themes/jova/templates/node--32.tpl.php
+++ sites/all/themes/jova/templates/node--32.tpl.php
@@ -3,6 +3,6 @@
 	<?php print render($content);?>  
   <div class="info-text">
 	  <p><?php print variable_get('grazinimai'); ?></p>
-    <a href="/grazinimo-salygos" class="button">Grąžinimo sąlygos</a>
+    <a href="/grąžinimo-sąlygos" class="button">Grąžinimo sąlygos</a>
   </div>
 </div>
