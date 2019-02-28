e6c91c8833e97e857f5d935a32f3bc42a3766d24
Minde Vieras <mindevieras@gmail.com>
2019-02-28 19:10:44 +0000

    grazinimai leftovers gone

--- sites/all/themes/jova/templates/node--32.tpl.php
+++
@@ -1,8 +0,0 @@
-
-<div class="grazinimai">
-	<?php print render($content);?>  
-  <div class="info-text">
-	  <p><?php print variable_get('grazinimai'); ?></p>
-    <a href="/grąžinimo-sąlygos" class="button">Grąžinimo sąlygos</a>
-  </div>
-</div>
